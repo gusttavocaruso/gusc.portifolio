53939d1479210d766626dbb416172e361bf74e3d
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-11-29 16:42:14 -0300

    new style

---
+++ script.js
@@ -0,0 +1,6 @@
+// document.querySelector('.itens')
+//   .addEventListener("wheel", (event) => (
+//     (event.deltaY > 0) ?
+//     event.target.scrollBy(150, 0) :
+//     event.target.scrollBy(-150, 0)
+// ));
